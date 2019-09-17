e8893831861dffbe06e689f3669ea2b9993c87ee
adrianoasg <adrianoasg@hotmail.com>
2019-09-17 13:28:47 -0300

    layout-BackOffice

--- module03/escamboapp/db/schema.rb
+++ module03/escamboapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_183804) do
+ActiveRecord::Schema.define(version: 2019_09_17_121748) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +27,17 @@ ActiveRecord::Schema.define(version: 2019_09_16_183804) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "ads", force: :cascade do |t|
+    t.string "title", limit: 255
+    t.text "description"
+    t.integer "member_id", null: false
+    t.string "category"
+    t.string "references"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["member_id"], name: "index_ads_on_member_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -52,4 +63,5 @@ ActiveRecord::Schema.define(version: 2019_09_16_183804) do
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ads", "members"
 end
