e4ee45c70ea3ebb3019775eaf456ee602f57d75f
adrianoasg <adrianoasg@hotmail.com>
2019-09-19 13:53:30 -0300

    add layout new and edit admins

--- module03/escamboapp/config/routes.rb
+++ module03/escamboapp/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  
+
   get 'backoffice', to: 'backoffice/dashboard#index'
 
   namespace :backoffice do
     resources :categories, except: [:show, :destroy]
+    resources :admins, except: [:show, :destroy]
     get 'dashboard', to: 'dashboard#index'
   end
 
@@ -11,7 +12,7 @@ Rails.application.routes.draw do
     get 'home', to: 'home#index'
   end
 
-  devise_for :admins
+  devise_for :admins, :skips => [:registration]
   devise_for :members
   
   root 'site/home#index'
