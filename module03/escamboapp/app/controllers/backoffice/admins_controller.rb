bbf9f9dd499332d68718f1ffb0092dea8b8b9170
adrianoasg <adrianoasg@hotmail.com>
2019-09-20 10:49:17 -0300

    add authorization with Pundit

--- module03/escamboapp/app/controllers/backoffice/admins_controller.rb
+++ module03/escamboapp/app/controllers/backoffice/admins_controller.rb
@@ -2,12 +2,14 @@ class Backoffice::AdminsController < BackofficeController
   before_action :set_admin, only: [:edit, :update, :destroy]
 
   def index
-    @admins = Admin.all
+    # @admins = Admin.all
     # @admins = Admin.whith_full_access
+    @admins = Admin.whith_restricted_access
   end
 
   def new 
     @admin = Admin.new
+    authorize @admin
   end
 
   def create
@@ -23,14 +25,6 @@ class Backoffice::AdminsController < BackofficeController
   end
 
   def update
-    pwd = params[:admin][:password]
-    pwd_confirmation = params[:admin][:password_confirmation]
-
-    if pwd.blank? && pwd_confirmation.blank?
-      params[:admin].delete(:password)
-      params[:admin].delete(:password_confirmation)
-    end  
-
     if @admin.update(params_admin)
       redirect_to backoffice_admins_path, notice: "O Administrador (#{@admin.email}) foi atualizada com sucesso!"
     else
@@ -54,7 +48,13 @@ class Backoffice::AdminsController < BackofficeController
   end
 
   def params_admin
+    pwd = params[:admin][:password]
+    pwd_confirmation = params[:admin][:password_confirmation]
+
+    if pwd.blank? && pwd_confirmation.blank?
+      params[:admin].except!(:password, :password_confirmation)
+    end  
+
     params.require(:admin).permit(:name, :email, :password, :password_confirmation)
   end
-
 end
--- module03/escamboapp/app/models/admin.rb
+++ module03/escamboapp/app/models/admin.rb
@@ -1,7 +1,12 @@
 class Admin < ActiveRecord::Base
-  enum role: {:full_access => 0, :restricted_access => 1}
+  # constants
+  ROLES = {:full_access => 0, :restricted_access => 1}
+  #enums
+  enum role: ROLES
 
-  scope :whith_full_access, -> { where(role: 'full_access') }
+  # scopes
+  scope :whith_full_access, -> { where(role: ROLES[:full_access]) }
+  scope :whith_restricted_access, -> { where(role: ROLES[:restricted_access]) }
 
   # def self.whith_full_access
   #   where(role: 'full_access')
@@ -12,11 +17,4 @@ class Admin < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  def role_br
-    if self.role == 'full_access'
-      'Acesso Completo'
-    else 
-      'Acesso Restrito'
-    end
-  end
 end
